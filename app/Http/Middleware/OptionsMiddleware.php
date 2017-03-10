9820a6daaaa840bdd803b5cb0c879880601da526
Murat <muratsatibaldiev@gmail.com>
2017-03-10 17:12:24 +0600

    update

--- app/Http/Middleware/OptionsMiddleware.php
+++ app/Http/Middleware/OptionsMiddleware.php
@@ -20,23 +20,8 @@ class OptionsMiddleware
     public function handle($request, Closure $next)
     {
         if ($request->method() === 'OPTIONS') {
-            //  $app = null;
-            //  $app = new Cors($app, array(
-            //      // you can use array('*') to allow any headers
-            //      'allowedHeaders' => array('*'),
-            //      // you can use array('*') to allow any methods
-            //      'allowedMethods' => array('*'),
-            //      // you can use array('*') to allow requests from any origin
-            //      'allowedOrigins' => array('*'),
-            //      'exposedHeaders' => false,
-            //      'maxAge' => false         ,
-            //      'supportsCredentials' => true,
-            //  ));
-//
-            // return $app;
-
             return response('', 200)
-                ->header('Access-Control-Allow-Origin', ' http://loc2.todo.kg')
+                ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Credentials', 'true')
                 ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
                 ->header('Access-Control-Allow-Headers', 'accept, content-type, x-xsrf-token, x-csrf-token');
