22020065f1b53f89b09582f73aef11caa2eed004
Murat <muratsatibaldiev@gmail.com>
2017-03-27 16:02:52 +0600

    use Repository pattern

--- app/Repositories/Contracts/TaskRepositoryInterface.php
+++ app/Repositories/Contracts/TaskRepositoryInterface.php
@@ -5,7 +5,10 @@ namespace App\Repositories\Contracts;
 interface TaskRepositoryInterface
 {
     public function all();
+
     public function update();
+
     public function create();
+
     public function delete();
 }
